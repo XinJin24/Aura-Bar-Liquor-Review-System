37d003f877e36f4f2b61bb51bdef03c31dc642fd
Xin Jin <jinxin8295@gmail.com>
2023-11-21 14:02:11 -0500

    strcture files and folder added

---
+++ config/mongoCollections.js
@@ -0,0 +1,20 @@
+import { dbConnection } from './mongoConnection.js';
+
+const getCollectionFn = (collection) => {
+    let _col = undefined;
+
+    return async () => {
+        if (!_col) {
+            const db = await dbConnection();
+            _col = await db.collection(collection);
+        }
+
+        return _col;
+    };
+};
+
+export const users = getCollectionFn('users');
+export const drinks = getCollectionFn('drinks');
+export const review = getCollectionFn('review');
+
+
