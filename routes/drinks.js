361a232b13048aec6b0e7aef6a4b7bcffd7d35eb
Xin Jin <jinxin8295@gmail.com>
2023-12-04 11:15:16 -0500

    repalce Ruikang's code to route/drinks.js

--- routes/drinks.js
+++ routes/drinks.js
@@ -4,6 +4,7 @@ import validation from "../publicMethods.js";
 import { getAllDrinkReservedByUserId, getAllReviewsByUserId, getUserIdByEmail } from "../data/users.js";
 import { getDrinkInfoByDrinkId } from "../data/drinks.js";
 import { getReviewInfoByReviewId } from "../data/reviews.js";
+import {createReview, deleteReview, updateReview} from "./data/reviews.js";
 
 //drink detail page and shows all reviews, if the review is made by the user, it will show the edit button
 router
@@ -74,6 +75,61 @@ router
         }
     })
     .post(async (req, res) => {
+        // Handle creating a new review
+        try {
+            const drinkId = validation.validateId(req.params.id, "ID");
+            const userId = getUserIdByEmail(req.session.user.email);
+            const { rating, comment } = req.body;
 
+            // Call the createReview function to insert the new review into the database
+            await createReview(userId, drinkId, rating, comment);
+
+            // Redirect or send response accordingly
+            return res.redirect(`/drinkInfo/${drinkId}`);
+        } catch (error) {
+            console.error(error);
+            return res.status(500).render('error', {
+                title: "Internal Server Error",
+                message: "Error: Internal Server Error"
+            });
+        }
+    })
+    .put(async (req, res) => {
+        // Handle updating an existing review
+        try {
+            const reviewId = validation.validateId(req.params.id, "ID");
+            const { rating, comment } = req.body;
+
+            // Call the updateReview function to update the review in the database
+            await updateReview(reviewId, { rating, comment });
+
+            // Redirect or send response accordingly
+            return res.redirect(`/drinkInfo/${drinkId}`);
+        } catch (error) {
+            console.error(error);
+            return res.status(500).render('error', {
+                title: "Internal Server Error",
+                message: "Error: Internal Server Error"
+            });
+        }
+    })
+    .delete(async (req, res) => {
+        // Handle deleting an existing review
+        try {
+            const reviewId = validation.validateId(req.params.id, "ID");
+
+            // Call the deleteReview function to delete the review from the database
+            await deleteReview(reviewId);
+
+            // Redirect or send response accordingly
+            return res.redirect('/drinkInfo'); // Redirect to drink info page or wherever appropriate
+        } catch (error) {
+            console.error(error);
+            return res.status(500).render('error', {
+                title: "Internal Server Error",
+                message: "Error: Internal Server Error"
+            });
+        }
     });
+
 export default router;
