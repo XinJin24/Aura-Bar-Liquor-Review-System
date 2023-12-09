0fff7406943c541e8c858bd59dfb21625c49bf74
Zimeng-Zhao <zmengzhao@foxmail.com>
2023-12-09 12:30:09 -0500

    correct the problem of register.js submit failure

--- public/js/login.js
+++ public/js/login.js
@@ -48,20 +48,25 @@ let checkPassword = (strVal)=>{
 // let valid1 = false;
 
 let login_form = document.getElementById("login_form");
-let emailAddressInput = document.getElementById(emailAddressInput);
-let passwordInput = document.getElementById(passwordInput);
+let emailAddressInput = document.getElementById("emailAddressInput");
+let passwordInput = document.getElementById("passwordInput");
 let error = document.getElementById('error')
 
+console.log(login_form);
+console.log(typeof login_form);
+console.log(login_form.submit);
+
+
 if(login_form){
-    login_form.addEventListener = ('submit', (event) =>{
+    login_form.addEventListener('submit', (event) =>{
         event.preventDefault();
         error.innerHTML = "";
         error.classList.add('hidden-div');
-        let emailAddressInput = emailAddressInput.value;
-        let passwordInput = passwordInput.value;
+        let emailAddress = emailAddressInput.value;
+        let password = passwordInput.value;
         try {
             emailAddress = checkEmail(emailAddress);
-        } catch (e) {
+        } catch (e){
             console.log(e);
             error.classList.remove('hidden-div');
             const msg = document.createElement('p');
@@ -80,16 +85,109 @@ if(login_form){
         }
 
         if (error.children.length === 0) {
-            login_form.submit()
+            login_form.submit();
         } else {
             return;
         }
     });
+    console.log(login_form.submit);
 }
 // login_form.addEventListener = ('submit', (event) =>{
 //     //stop default behaviour of the form submission
 //     if(!valid1){
+//         event.preventDefault();let checkEmail = (strVal) =>{
+//     if(!strVal){
+//         throw `email is not provided`;
+//     }
+//     strVal = strVal.trim();
+//     if(typeof strVal !== "string" || strVal.length === 0){
+//         throw `email is not strings or is empty strings`;
+//     }
+//     let validEmail = /^[A-Za-z0-9_.-]+@[A-Za-z0-9-]+\.[A-Za-z]{2,}$/;
+//     if(!validEmail.test(strVal)){
+//         throw `email is not a valid email address`;
+//     }
+//     strVal = strVal.toLowerCase();
+//     return strVal;
+// }
+//
+// let checkPassword = (strVal)=>{
+//     if(!strVal){
+//         throw `password is not provided`;
+//     }
+//     strVal = strVal.trim();
+//     if(typeof strVal !== "string" || strVal.length === 0){
+//         throw `password is not strings or is empty strings`;
+//     }
+//     if(strVal.length < 8){
+//         throw `password should be a minimum of 8 characters long`;
+//     }
+//     if(!/A-Z/){
+//         throw `password needs to be at least one uppercase character`;
+//     }
+//     if(!/\d/){
+//         throw `password needs to be at least one number `;
+//     }
+//     if(!/[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(strVal)){
+//         throw `password needs to be at least one special character`;
+//     }
+//     for(let i = 0; i < strVal.length; i++){
+//         if(strVal.charAt(i) === ''){
+//             throw `password shouldn't have spaces`;
+//         }
+//     }
+//     return strVal;
+// }
+//
+// // let clientError1 = document.getElementById("clientError1");
+// // clientError1.style.display = 'none';
+// // let valid1 = false;
+//
+// let login_form = document.getElementById("login_form");
+// let emailAddressInput = document.getElementById("emailAddressInput");
+// let passwordInput = document.getElementById("passwordInput");
+// let error = document.getElementById('error')
+//
+// console.log(login_form);
+// console.log(typeof login_form);
+// console.log(login_form.submit);
+//
+//
+// if(login_form){
+//     login_form.addEventListener('submit', (event) =>{
 //         event.preventDefault();
+//         error.innerHTML = "";
+//         error.classList.add('hidden-div');
+//         let emailAddress = emailAddressInput.value;
+//         let password = passwordInput.value;
+//         try {
+//             emailAddress = checkEmail(emailAddress);
+//         } catch (e){
+//             console.log(e);
+//             error.classList.remove('hidden-div');
+//             const msg = document.createElement('p');
+//             msg.innerHTML = e;
+//             error.appendChild(msg);
+//         }
+//
+//         try {
+//             password = checkPassword(password);
+//         } catch (e) {
+//             console.log(e);
+//             error.classList.remove('hidden-div');
+//             const msg = document.createElement('p');
+//             msg.innerHTML = e;
+//             error.appendChild(msg);
+//         }
+//
+//         if (error.children.length === 0) {
+//             login_form.submit();
+//         } else {
+//             return;
+//         }
+//     });
+//     console.log(login_form.submit);
+// }
 //         try{
 //             if(emailAddressInput){
 //                 checkEmail(emailAddressInput.value);
--- public/js/register.js
+++ public/js/register.js
@@ -10,8 +10,8 @@ let checkName = (strVal) =>{
         throw `name should be at least 2 characters long with a max of 25 characters`;
     }
     if (!isNaN(strVal))
-      throw `${strVal} is not a valid value for name as it only contains digits`;
-    return strVal;
+        throw `${strVal} is not a valid value for name as it only contains digits`;
+    // return strVal;
 }
 
 let checkEmail = (strVal) =>{
@@ -27,7 +27,7 @@ let checkEmail = (strVal) =>{
         throw `email is not a valid email address`;
     }
     strVal = strVal.toLowerCase();
-    return strVal;
+    // return strVal;
 }
 
 let checkPassword = (password) =>{
@@ -55,7 +55,7 @@ let checkPassword = (password) =>{
     if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
         throw `Error: password must contain at least one special character`;
     }
-    return password;
+    // return password;
 }
 
 let checkConfirm = (code1,code2) =>{
@@ -75,191 +75,224 @@ let checkRole = (strVal) =>{
     if(strVal !== "admin" && strVal !== "user"){
         throw `role the ONLY two valid values are "admin" or "user"`;
     }
-    return strVal;
+    // return strVal;
 }
 
 let checkPhoneNumber = (phoneNumber) =>{
     if (!phoneNumber) {
-        throw "Error: Phone number not supplied";
+        throw `Phone number not supplied`;
     }
     if (typeof phoneNumber !== "string" || phoneNumber.trim().length === 0) {
-        throw "Error: Phone number should be a valid string (no empty spaces)";
+        throw `Phone number should be a valid string (no empty spaces)`;
     }
     phoneNumber = phoneNumber.trim();
     const phoneRegex = /^(\+\d{1,2}\s?)?(\d{1,4}\s?)?[\d\s-]+$/;
     if (!phoneRegex.test(phoneNumber)) {
-        throw "Error: Invalid phone number format";
+        throw `Invalid phone number format`;
     }
-    return phoneNumber;
+    // return phoneNumber;
 }
+
+let valid1 = false;
+$('#error').hide()
+$('#registration_form').submit((event)=>{
+    if(!valid1)
+    {
+        event.preventDefault();
+        try{
+            if($('#firstNameInput').length)
+                checkName($('#firstNameInput').val())
+            if($('#lastNameInput').length)
+                checkName($('#lastNameInput').val())
+            if($('#emailAddressInput').length)
+                checkEmail($('#emailAddressInput').val());
+            if($('#phoneNumberInput').length)
+                checkPhoneNumber($('#phoneNumberInput').val())
+            if($('#passwordInput').length)
+                checkPassword($('#passwordInput').val());
+            if($('#confirmPasswordInput').length)
+                checkConfirm($('#passwordInput').val(),$('#confirmPasswordInput').val())
+            if($('#roleInput').length)
+                checkRole($('#roleInput').val())
+            valid1=true;
+            $('#registration_form').submit();//submit失效，若删除event.preventDefault();前端验证失效，若改submit id，前端验证失效
+            // $('#registration_form')[0].submit();
+            valid1=false;
+        }catch(e){
+            $('#error').hide();
+            $('#error').append(`<p>${e}</p>`);
+            $('#error').show();
+        }
+    }
+})
 
 // let clientError2 = document.getElementById("clientError2");
 // clientError2.style.display = 'none';
 // let valid2 = false;
 
-let firstNameInput = document.getElementById('firstNameInput');
-let lastNameInput = document.getElementById('lastNameInput');
-let emailAddressInput = document.getElementById('emailAddressInput');
-let phoneNumberInput = document.getElementById('phoneNumberInput');
-let passwordInput = document.getElementById('passwordInput');
-let confirmPasswordInput = document.getElementById('confirmPasswordInput');
-let roleInput = document.getElementById('roleInput');
-let error = document.getElementById('error')
-let registration_form = document.getElementById('registration-form');
-
-
-
-if(registration_form){
-    registration_form.addEventListener('submit', (event)=>{
-        // event.preventDefault();
-        error.innerHTML = "";
-        error.classList.add('hidden-div');
-        let firstName = firstNameInput.value;
-        let lastName = lastNameInput.value;
-        let emailAddress = emailAddressInput.value;
-        let phoneNumberInput = phoneNumberInput.value;
-        let password = passwordInput.value;
-        let confirmPassword = confirmPasswordInput.value;
-        let role = roleInput.value;
-
-        try {
-            firstName = checkName(firstName);
-        } catch (e) {
-            console.log(e);
-            error.classList.remove('hidden-div');
-            const msg = document.createElement('p');
-            msg.innerHTML = e;
-            error.appendChild(msg);
-        }
-
-        try {
-            lastName = checkName(lastName);
-        } catch (e) {
-            console.log(e);
-            error.classList.remove('hidden-div');
-            const msg = document.createElement('p');
-            msg.innerHTML = e;
-            error.appendChild(msg);
-        }
-
-        try {
-            emailAddress = checkEmail(emailAddress);
-        } catch (e) {
-            console.log(e);
-            error.classList.remove('hidden-div');
-            const msg = document.createElement('p');
-            msg.innerHTML = e;
-            error.appendChild(msg);
-        }
-
-        try {
-            phoneNumberInput = checkPhoneNumber(phoneNumberInput);
-        } catch (e) {
-            console.log(e);
-            error.classList.remove('hidden-div');
-            const msg = document.createElement('p');
-            msg.innerHTML = e;
-            error.appendChild(msg);
-        }
-
-        try {
-            password = checkPassword(password);
-        } catch (e) {
-            console.log(e);
-            error.classList.remove('hidden-div');
-            const msg = document.createElement('p');
-            msg.innerHTML = e;
-            error.appendChild(msg);
-        }
-
-
-        try {
-            confirmPassword = checkPassword(confirmPassword);
-        } catch (e) {
-            console.log(e);
-            error.classList.remove('hidden-div');
-            const msg = document.createElement('p');
-            msg.innerHTML = e;
-            error.appendChild(msg);
-        }
-
-        try {
-            checkConfirm(password, confirmPassword);
-        } catch (e) {
-            console.log(e);
-            error.classList.remove('hidden-div');
-            const msg = document.createElement('p');
-            msg.innerHTML = e;
-            error.appendChild(msg);
-        }
-
-        try {
-            role = checkRole(role);
-        } catch (e) {
-            console.log(e);
-            error.classList.remove('hidden-div');
-            const msg = document.createElement('p');
-            msg.innerHTML = e;
-            error.appendChild(msg);
-        }
-        if (error.children.length === 0) {
-            registration_form.submit()
-        } else {
-            return;
-        }
-    });
-}
-
-
-const checkFile = (file, errorArr) => {
-
-    if (!file) {
-        errorArr.push("No image input");
-        return;
-    }
-    if (file.size >= 16777216) {
-        errorArr.push("Image Size should smaller than 16mb ");
-        return;
-    }
-    if (file.type !== "image/jpeg" && file.type !== "image/png") {
-        errorArr.push("Image type should be jpeg or png ");
-        return;
-    }
-    return;
-}
-
-
-let photoInput = document.getElementById('photoInput');
-let formReady = true;
-
-document.getElementById('photoInput').addEventListener('change', function (event) {
-    const errorMsg = [];
-    document.getElementById('fileError').innerHTML = '';
-
-    const fileInput = event.target;
-    const file = fileInput.files[0];
-
-    if (!file) {
-        formReady = true;
-        document.getElementById('previewImg').src = ''; // Clear the preview image
-        return;
-    }
-
-    console.log(file);
-    checkFile(file, errorMsg);
-
-    if (errorMsg.length > 0) {
-        formReady = false;
-        errorMsg.forEach(msg => {
-            const li = document.createElement('li');
-            li.textContent = msg;
-            li.style.color = 'red';
-            document.getElementById('fileError').appendChild(li);
-        });
-    } else {
-        formReady = true;
-    }
-});
+// let firstNameInput = document.getElementById('firstNameInput');
+// let lastNameInput = document.getElementById('lastNameInput');
+// let emailAddressInput = document.getElementById('emailAddressInput');
+// let phoneNumberInput = document.getElementById('phoneNumberInput');
+// let passwordInput = document.getElementById('passwordInput');
+// let confirmPasswordInput = document.getElementById('confirmPasswordInput');
+// let roleInput = document.getElementById('roleInput');
+// let error = document.getElementById('error')
+// let registration_form = document.getElementById('registration-form');
+//
+//
+//
+// if(registration_form){
+//     registration_form.addEventListener('submit', (event)=>{
+//         // event.preventDefault();
+//         error.innerHTML = "";
+//         error.classList.add('hidden-div');
+//         let firstName = firstNameInput.value;
+//         let lastName = lastNameInput.value;
+//         let emailAddress = emailAddressInput.value;
+//         let phoneNumberInput = phoneNumberInput.value;
+//         let password = passwordInput.value;
+//         let confirmPassword = confirmPasswordInput.value;
+//         let role = roleInput.value;
+//
+//         try {
+//             firstName = checkName(firstName);
+//         } catch (e) {
+//             console.log(e);
+//             error.classList.remove('hidden-div');
+//             const msg = document.createElement('p');
+//             msg.innerHTML = e;
+//             error.appendChild(msg);
+//         }
+//
+//         try {
+//             lastName = checkName(lastName);
+//         } catch (e) {
+//             console.log(e);
+//             error.classList.remove('hidden-div');
+//             const msg = document.createElement('p');
+//             msg.innerHTML = e;
+//             error.appendChild(msg);
+//         }
+//
+//         try {
+//             emailAddress = checkEmail(emailAddress);
+//         } catch (e) {
+//             console.log(e);
+//             error.classList.remove('hidden-div');
+//             const msg = document.createElement('p');
+//             msg.innerHTML = e;
+//             error.appendChild(msg);
+//         }
+//
+//         try {
+//             phoneNumberInput = checkPhoneNumber(phoneNumberInput);
+//         } catch (e) {
+//             console.log(e);
+//             error.classList.remove('hidden-div');
+//             const msg = document.createElement('p');
+//             msg.innerHTML = e;
+//             error.appendChild(msg);
+//         }
+//
+//         try {
+//             password = checkPassword(password);
+//         } catch (e) {
+//             console.log(e);
+//             error.classList.remove('hidden-div');
+//             const msg = document.createElement('p');
+//             msg.innerHTML = e;
+//             error.appendChild(msg);
+//         }
+//
+//
+//         try {
+//             confirmPassword = checkPassword(confirmPassword);
+//         } catch (e) {
+//             console.log(e);
+//             error.classList.remove('hidden-div');
+//             const msg = document.createElement('p');
+//             msg.innerHTML = e;
+//             error.appendChild(msg);
+//         }
+//
+//         try {
+//             checkConfirm(password, confirmPassword);
+//         } catch (e) {
+//             console.log(e);
+//             error.classList.remove('hidden-div');
+//             const msg = document.createElement('p');
+//             msg.innerHTML = e;
+//             error.appendChild(msg);
+//         }
+//
+//         try {
+//             role = checkRole(role);
+//         } catch (e) {
+//             console.log(e);
+//             error.classList.remove('hidden-div');
+//             const msg = document.createElement('p');
+//             msg.innerHTML = e;
+//             error.appendChild(msg);
+//         }
+//         if (error.children.length === 0) {
+//             registration_form.submit()
+//         } else {
+//             return;
+//         }
+//     });
+// }
+//
+//
+// const checkFile = (file, errorArr) => {
+//
+//     if (!file) {
+//         errorArr.push("No image input");
+//         return;
+//     }
+//     if (file.size >= 16777216) {
+//         errorArr.push("Image Size should smaller than 16mb ");
+//         return;
+//     }
+//     if (file.type !== "image/jpeg" && file.type !== "image/png") {
+//         errorArr.push("Image type should be jpeg or png ");
+//         return;
+//     }
+//     return;
+// }
+//
+//
+// let photoInput = document.getElementById('photoInput');
+// let formReady = true;
+//
+// document.getElementById('photoInput').addEventListener('change', function (event) {
+//     const errorMsg = [];
+//     document.getElementById('fileError').innerHTML = '';
+//
+//     const fileInput = event.target;
+//     const file = fileInput.files[0];
+//
+//     if (!file) {
+//         formReady = true;
+//         document.getElementById('previewImg').src = ''; // Clear the preview image
+//         return;
+//     }
+//
+//     console.log(file);
+//     checkFile(file, errorMsg);
+//
+//     if (errorMsg.length > 0) {
+//         formReady = false;
+//         errorMsg.forEach(msg => {
+//             const li = document.createElement('li');
+//             li.textContent = msg;
+//             li.style.color = 'red';
+//             document.getElementById('fileError').appendChild(li);
+//         });
+//     } else {
+//         formReady = true;
+//     }
+// });
 
 
 
