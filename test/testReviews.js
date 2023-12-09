7c64b0f2c4f64f5ef18f1d624f6057ccf818dbbe
Ruikang Li <84649626+liruikang0126@users.noreply.github.com>
2023-12-08 21:10:42 -0500

    tests for drink.js and review.js

--- test/testReviews.js
+++ test/testReviews.js
@@ -3,3 +3,50 @@ console.log(validation.validatePhoneNumber("19293428295"));
 
 
 console.log(await validation.validateIfFileExist("C:\\Users\\jinxi\\git_repos\\Aura-Bar-Liquor-Review-System\\public\\userProfilePictures\\636m209480uxu55xaw4cxj.jpg"));
+
+
+// // Test Case 1: createReview
+// try {
+//     const result = await createReview(
+//         "6570d5425adfaacae3e405ec", // drinkId
+//         "userTestId", // userId
+//         "Great drink! Highly recommended.", // reviewText
+//         5, // rating
+//         "C:\\Users\\jinxi\\git_repos\\Aura-Bar-Liquor-Review-System\\public\\reviewPictures\\1.jpg" // reviewPictureLocation
+//     );
+//     console.log(result); // Log the result
+// } catch (error) {
+//     console.error(error); // Log any errors
+// }
+
+// // Test Case 2: updateReview
+// try {
+//     const result = await updateReview(
+//         "6570d5425adfaacae3e405ec", // reviewId
+//         "2023-11-30T12:30:00", // timeStamp
+//         "6570d5425adfaacae3e405ec", // drinkId
+//         "userTestId", // userId
+//         "Updated review text.", // reviewText
+//         4, // rating
+//         "C:\\Users\\jinxi\\git_repos\\Aura-Bar-Liquor-Review-System\\public\\reviewPictures\\2.jpg" // reviewPictureLocation
+//     );
+//     console.log(result); // Log the result
+// } catch (error) {
+//     console.error(error); // Log any errors
+// }
+
+// // Test Case 3: deleteReview
+// try {
+//     const result = await deleteReview("6570d5425adfaacae3e405ec"); // reviewId
+//     console.log(result); // Log the result
+// } catch (error) {
+//     console.error(error); // Log any errors
+// }
+
+// // Test Case 4: getReviewInfoByReviewId
+// try {
+//     const result = await getReviewInfoByReviewId("6570d5425adfaacae3e405ec"); // reviewId
+//     console.log(result); // Log the result
+// } catch (error) {
+//     console.error(error); // Log any errors
+// }
